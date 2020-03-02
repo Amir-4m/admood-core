4b157617f72c6a0962f2e19db0e30dd5ca07c7e0
mehdi taherifard <m.taherifard@hami.blue>
2020-03-02 17:02:42 +0330

    add created_time updated_time

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -7,23 +7,38 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=36, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
-    first_name = models.CharField(max_length=30, blank=True, null=True)
-    last_name = models.CharField(max_length=30, blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(unique=True, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
-    USERNAME_FIELD = 'phone_number'
+    USERNAME_FIELD = "username"
 
     def __str__(self):
         return self.phone_number
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    REAL = "real"
+    LEGAL = "legal"
+
+    TYPE_CHOICES = (
+        (REAL, "real"),
+        (LEGAL, "legal"),
+    )
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES, default=REAL)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user
