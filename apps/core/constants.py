4b28f5ff1d25a33664525879d8d3bb20180f0d3d
mehdi taherifard <m.taherifard@hami.blue>
2020-03-04 09:23:55 +0330

    add MediumType and MediumInterface

---
+++ apps/core/constants.py
@@ -0,0 +1,30 @@
+class MediumType:
+    TELEGRAM = "Telegram"
+    INSTAGRAM = "Instagram"
+    PUSH_NOTIF = "PushNotif"
+    BANNER = "Banner"
+    NATIVE = "Native"
+
+    MEDIUM_TYPE_CHOICES = (
+        (TELEGRAM, "Telegram"),
+        (INSTAGRAM, "Instagram"),
+        (PUSH_NOTIF, "PushNotif"),
+        (BANNER, "Banner"),
+        (NATIVE, "Native"),
+    )
+
+
+class MediumInterface:
+    WEB = "Web"
+    IN_APP = "InAPP"
+    CHANNEL = "Channel"
+    POST = "Post"
+    STORY = "Story"
+
+    MEDIUM_INTERFACE_CHOICES = (
+        (WEB, "Web"),
+        (IN_APP, "InAPP"),
+        (CHANNEL, "Channel"),
+        (POST, "Post"),
+        (STORY, "Story"),
+    )
