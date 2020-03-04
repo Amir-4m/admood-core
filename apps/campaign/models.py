3ff73aaf6585fb4695bac67a05baf1363cb23b73
mehdi taherifard <m.taherifard@hami.blue>
2020-03-04 14:20:33 +0330

    remove campaign app

--- apps/campaign/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from admood_core import settings
-from apps.core.constants import MediumType, ServiceProvider
-from apps.core.models import OS
-from apps.publish.models import Publisher
-
-
