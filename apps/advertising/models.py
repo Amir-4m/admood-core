da47b1b0c5bf249e85d50d925efe22911e8dbc85
mehdi taherifard <m.taherifard@hami.blue>
2020-03-04 14:18:45 +0330

    move Publisher and Campaign model to advertising app add CampaignContent model

--- apps/advertising/models.py
+++ apps/advertising/models.py
@@ -1,3 +1,50 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
+from admood_core import settings
+from apps.core.constants import MediumType, ServiceProvider
 
-# Create your models here.
+
+class Publisher(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    medium_type = models.CharField(max_length=20, choices=MediumType.MEDIUM_TYPE_CHOICES)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    status = models.CharField(max_length=10)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class Campaign(models.Model):
+    advertiser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    medium_type = models.CharField(max_length=20, choices=MediumType.MEDIUM_TYPE_CHOICES)
+    service_provider = models.CharField(max_length=10, choices=ServiceProvider.SERVICE_PROVIDER_CHOICES)
+    os = models.ForeignKey(OS, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True, blank=True)
+    utm_source = models.CharField(max_length=50, null=True, blank=True)
+    utm_medium = models.CharField(max_length=50, null=True, blank=True)
+    utm_campaign = models.CharField(max_length=50, null=True, blank=True)
+    utm_content = models.CharField(max_length=50, null=True, blank=True)
+    cpm = models.CharField(max_length=50, null=True, blank=True)
+    cpc = models.CharField(max_length=50, null=True, blank=True)
+    cpa = models.CharField(max_length=50, null=True, blank=True)
+    daily_cost = models.IntegerField()
+    total_cost = models.IntegerField()
+    finish_balance = models.IntegerField(null=True, blank=True)
+    campaign_status = models.CharField(max_length=20)
+    is_enabled = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class CampaignContent(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    description = models.TextField()
+    data = JSONField()
+    landing_url = models.URLField()
+    utm_term = models.CharField(max_length=100)
+    image = models.ImageField()
--- apps/campaign/models.py
+++ apps/campaign/models.py
@@ -1,33 +1,8 @@
 from django.db import models
 
 from admood_core import settings
-from apps.core.constants import MediumType
+from apps.core.constants import MediumType, ServiceProvider
+from apps.core.models import OS
 from apps.publish.models import Publisher
 
 
-class Campaign(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    medium_type = models.CharField(max_length=20, choices=MediumType.MEDIUM_TYPE_CHOICES)
-    # objective =
-    # ad =
-    # schedule =
-    # device =
-    # location =
-    name = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField(null=True, blank=True)
-    utm_source = models.CharField(max_length=50, null=True, blank=True)
-    utm_medium = models.CharField(max_length=50, null=True, blank=True)
-    utm_campaign = models.CharField(max_length=50, null=True, blank=True)
-    utm_content = models.CharField(max_length=50, null=True, blank=True)
-    cpm = models.CharField(max_length=50, null=True, blank=True)
-    cpc = models.CharField(max_length=50, null=True, blank=True)
-    cpa = models.CharField(max_length=50, null=True, blank=True)
-    daily_cost = models.DecimalField(max_digits=20, decimal_places=2)
-    total_cost = models.DecimalField(max_digits=20, decimal_places=2)
-    finish_balance = models.DecimalField(max_digits=20, decimal_places=2)
-    campaign_status = models.CharField(max_length=20)
-    created_time = models.DateTimeField(auto_now_add=True)
-    is_enabled = models.BooleanField(default=True)
