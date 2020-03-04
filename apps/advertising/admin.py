4b88fe4c174940f733496b6817bce90bf5f4ac13
mehdi taherifard <m.taherifard@hami.blue>
2020-03-04 16:24:23 +0330

    add advertising admin

--- apps/advertising/admin.py
+++ apps/advertising/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    MediumCategoryDisplayText,
+    Platform,
+    OS,
+    OSVersion,
+    Province,
+    Campaign,
+    CampaignTargetDevice,
+    CampaignContent,
+    Publisher
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(MediumCategoryDisplayText)
+admin.site.register(Platform)
+admin.site.register(OS)
+admin.site.register(OSVersion)
+admin.site.register(Province)
+admin.site.register(Campaign)
+admin.site.register(CampaignTargetDevice)
+admin.site.register(CampaignContent)
+admin.site.register(Publisher)
