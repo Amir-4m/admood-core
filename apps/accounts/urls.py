7791a3b7648e40129b760914f91678a0a08c6401
mehdi taherifard <m.taherifard@hami.blue>
2020-03-11 17:36:23 +0330

    fix google url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import HelloView, GoogleView
+from .views import HelloView, GoogleAPIView
 
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(), name="token"),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
-    path('google/', GoogleView.as_view(), name="google"),
+    path('google/', GoogleAPIView.as_view(), name="google"),
     path('hello/', HelloView.as_view(), name="hello"),
 ]
