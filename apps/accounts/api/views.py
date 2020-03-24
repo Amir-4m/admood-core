28b2d0c08dc036629ef13228a2b3649d5210d4e1
mehdi taherifard <m.taherifard@hami.blue>
2020-03-24 18:36:06 +0430

    remove GoogleAPIView

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -1,19 +1,11 @@
-import requests
-
-from django.contrib.auth import get_user_model
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.hashers import make_password
-
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.utils import json
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-
-User = get_user_model()
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class HelloView(APIView):
+    # authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
@@ -32,36 +24,3 @@ class LoginAPIView(APIView):
     def post(self, request):
         pass
 
-
-class ConfirmLoginAPIView(APIView):
-    def post(self, request):
-        pass
-
-
-class GoogleAPIView(APIView):
-    def post(self, request):
-        payload = {'access_token': request.data.get("token")}  # validate the token
-        r = requests.get('https://www.googleapis.com/oauth2/v2/userinfo', params=payload)
-        data = json.loads(r.text)
-
-        if 'error' in data:
-            content = {'message': 'invalid google token'}
-            return Response(content)
-
-        # create user if not exist
-        try:
-            user = User.objects.get(email=data['email'])
-        except User.DoesNotExist:
-            user = User()
-            user.username = data['email']
-            # provider random default password
-            user.password = make_password(BaseUserManager().make_random_password())
-            user.email = data['email']
-            user.save()
-
-        token = RefreshToken.for_user(user)  # generate token without username & password
-        data = {
-            'access_token': str(token.access_token),
-            'refresh_token': str(token)
-        }
-        return Response(data=data)
