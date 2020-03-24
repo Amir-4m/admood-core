0c4075b9e94bda36d66f3b04b54338327f8d8c34
mehdi taherifard <m.taherifard@hami.blue>
2020-03-24 15:59:56 +0430

    remove REST_FRAMEWORK DEFAULT_AUTHENTICATION_CLASSES

--- admood_core/settings.py
+++ admood_core/settings.py
@@ -133,11 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
 
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True
