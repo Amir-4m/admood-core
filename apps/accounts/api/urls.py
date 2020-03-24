8b7910d78a423d2acc6618bddef2f5f610ab948d
mehdi taherifard <m.taherifard@hami.blue>
2020-03-24 18:35:48 +0430

    remove google url

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import HelloView, GoogleAPIView
+from .views import HelloView
 
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name="token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 
-    path('google/', GoogleAPIView.as_view(), name="google"),
     path('hello/', HelloView.as_view(), name="hello"),
 ]
