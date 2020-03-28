c06f855cd681bce6979447b707b8428b1c16f095
mehdi taherifard <m.taherifard@hami.blue>
2020-03-28 17:36:24 +0430

    update campaign admin

--- apps/campaign/admin.py
+++ apps/campaign/admin.py
@@ -6,7 +6,16 @@ from .models import (
     CampaignContent,
 )
 
+
+class TargetDeviceAdmin(admin.ModelAdmin):
+    list_display = ("campaign", "platform", "os", "os_version", "service_provider")
+
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("campaign", "title", "subtitle", "data")
+
+
 admin.site.register(Province)
 admin.site.register(Campaign)
-admin.site.register(CampaignTargetDevice)
-admin.site.register(CampaignContent)
+admin.site.register(CampaignTargetDevice, TargetDeviceAdmin)
+admin.site.register(CampaignContent, ContentAdmin)
