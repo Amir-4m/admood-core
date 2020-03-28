17815fe0bfd0c8f23c5a73359b82b15bfa8c1cb4
mehdi taherifard <m.taherifard@hami.blue>
2020-03-28 17:36:47 +0430

    add __str__ for device models

--- apps/device/models.py
+++ apps/device/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Platform(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class OS(models.Model):
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
@@ -13,6 +16,9 @@ class OS(models.Model):
         verbose_name = "OS"
         verbose_name_plural = "OS"
 
+    def __str__(self):
+        return self.name
+
 
 class OSVersion(models.Model):
     os = models.ForeignKey(OS, on_delete=models.CASCADE)
@@ -21,3 +27,6 @@ class OSVersion(models.Model):
     class Meta:
         verbose_name = "OS Version"
         verbose_name_plural = "OS Versions"
+
+    def __str__(self):
+        return self.version
