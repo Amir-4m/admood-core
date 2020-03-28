2cbaa52ba4fb1c69d7148748414e9312ba7fb6ef
mehdi taherifard <m.taherifard@hami.blue>
2020-03-28 17:39:42 +0430

    add campaign router and urls

--- apps/campaign/api/urls.py
+++ apps/campaign/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import CampaignListCreateAPIView
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('campaigns/', CampaignListCreateAPIView.as_view(), name="token"),
-]
+from .views import ProvinceViewSet, CampaignViewSet, TargetDeviceViewSet, ContentViewSet
+
+router = DefaultRouter()
+router.register('provinces', ProvinceViewSet)
+router.register('campaigns', CampaignViewSet)
+router.register(prefix=r'(?P<campaign_id>\d+)/target_devices', viewset=TargetDeviceViewSet, basename='CampaignContent')
+router.register('contents', ContentViewSet)
+
+urlpatterns = router.urls
