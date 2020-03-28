533c21ad0ab98ea35075fb62170048f8e4cd9142
mehdi taherifard <m.taherifard@hami.blue>
2020-03-28 17:36:07 +0430

    update device admin

--- apps/device/admin.py
+++ apps/device/admin.py
@@ -5,6 +5,18 @@ from .models import (
     OSVersion,
 )
 
+
+class OSAdmin(admin.ModelAdmin):
+    list_display = ("platform", "name")
+
+
+class OSVersionAdmin(admin.ModelAdmin):
+    list_display = ("platform", "os", "version")
+
+    def platform(self, obj):
+        return obj.os.platform
+
+
 admin.site.register(Platform)
-admin.site.register(OS)
-admin.site.register(OSVersion)
+admin.site.register(OS, OSAdmin)
+admin.site.register(OSVersion, OSVersionAdmin)
