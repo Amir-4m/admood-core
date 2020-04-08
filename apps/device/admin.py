6e0be65a6ee1988f54e3433d3a4f21586d0bb024
mehdi taherifard <m.taherifard@hami.blue>
2020-04-08 10:40:34 +0430

    comment device app admin file

--- apps/device/admin.py
+++ apps/device/admin.py
@@ -1,22 +1,22 @@
-from django.contrib import admin
-from .models import (
-    Platform,
-    OS,
-    OSVersion,
-)
-
-
-class OSAdmin(admin.ModelAdmin):
-    list_display = ("platform", "name")
-
-
-class OSVersionAdmin(admin.ModelAdmin):
-    list_display = ("platform", "os", "version")
-
-    def platform(self, obj):
-        return obj.os.platform
-
-
-admin.site.register(Platform)
-admin.site.register(OS, OSAdmin)
-admin.site.register(OSVersion, OSVersionAdmin)
+# from django.contrib import admin
+# from .models import (
+#     Platform,
+#     OS,
+#     OSVersion,
+# )
+#
+#
+# class OSAdmin(admin.ModelAdmin):
+#     list_display = ("platform", "name")
+#
+#
+# class OSVersionAdmin(admin.ModelAdmin):
+#     list_display = ("platform", "os", "version")
+#
+#     def platform(self, obj):
+#         return obj.os.platform
+#
+#
+# admin.site.register(Platform)
+# admin.site.register(OS, OSAdmin)
+# admin.site.register(OSVersion, OSVersionAdmin)
