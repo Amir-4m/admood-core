3cf3d075f6e0410fb74a5641ee7d71176a293c3b
mehdi taherifard <m.taherifard@hami.blue>
2020-04-18 11:10:18 +0430

    GoogleAuthBackend to google.oauth2

--- apps/accounts/backends.py
+++ apps/accounts/backends.py
@@ -3,7 +3,8 @@ import logging
 import requests
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
-from rest_framework.utils import json
+from google.auth.transport import requests
+from google.oauth2 import id_token
 
 User = get_user_model()
 logger = logging.getLogger('admood_core.accounts')
@@ -11,25 +12,30 @@ logger = logging.getLogger('admood_core.accounts')
 
 class GoogleAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        payload = {'access_token': password}  # validate the token
-        r = requests.get('https://www.googleapis.com/oauth2/v2/userinfo', params=payload)
-        data = json.loads(r.text)
-
-        if 'error' in data:
-            logger.error(data["error"])
-            return
-
-        if username != data["email"]:
-            logger.error("email is not match")
-            return
-
         try:
-            user = User.objects.get(email=data['email'])
+            id_info = id_token.verify_oauth2_token(password, requests.Request())
 
-        # create user if not exist
-        except User.DoesNotExist:
-            user = User.objects.create_user(
-                email=data["email"],
-            )
+            if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise ValueError('Wrong issuer.')
 
-        return user
+            email = id_info['email']
+
+            if username != email:
+                logger.error("Email is not match")
+                return
+
+            try:
+                user = User.objects.get(email=email)
+
+            # Create user if not exist
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    email=email,
+                )
+
+            return user
+
+        except ValueError:
+            logger.error("Invalid google token.")
+            # Invalid token
+            pass
