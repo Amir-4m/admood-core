20158dba3c2468a187efb1c6de154019768310b4
mehdi taherifard <m.taherifard@hami.blue>
2020-04-20 11:06:33 +0430

    reformat google backend auth

--- apps/accounts/backends.py
+++ apps/accounts/backends.py
@@ -1,10 +1,9 @@
+import json
 import logging
 
 import requests
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
-from google.auth.transport import requests
-from google.oauth2 import id_token
 
 User = get_user_model()
 logger = logging.getLogger('admood_core.accounts')
@@ -13,16 +12,15 @@ logger = logging.getLogger('admood_core.accounts')
 class GoogleAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            id_info = id_token.verify_oauth2_token(password, requests.Request())
+            response = requests.get("https://oauth2.googleapis.com/tokeninfo", params={"id_token": password})
+            data = json.loads(response.text)
+            email = data['email']
 
-            if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            if data['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
                 raise ValueError('Wrong issuer.')
 
-            email = id_info['email']
-
             if username != email:
-                logger.error("Email is not match")
-                return
+                raise ValueError('Wrong email.')
 
             try:
                 user = User.objects.get(email=email)
@@ -32,7 +30,6 @@ class GoogleAuthBackend(ModelBackend):
                 user = User.objects.create_user(
                     email=email,
                 )
-
             return user
 
         except ValueError:
