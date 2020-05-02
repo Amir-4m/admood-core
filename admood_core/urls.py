ab79e00c73f4d0fa4eb76c38a68e42f60280c5c3
mehdi taherifard <m.taherifard@hami.blue>
2020-05-02 15:23:45 +0430

    handle media files urls

--- admood_core/urls.py
+++ admood_core/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+from admood_core import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,5 +40,7 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     path('api/v1/', include('apps.urls_api')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
