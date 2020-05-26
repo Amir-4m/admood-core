b8fbea1e47f7674352db5d6bd4ed002a1d7497e2
mehdi taherifard <m.taherifard@hami.blue>
2020-05-26 16:16:31 +0430

    add profile partial update url

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from .views import TokenObtainPairView, TokenRefreshView, UserProfileViewSet, register, activate
+from apps.accounts.api.views import confirm_registration_code
+from .views import TokenObtainPairView, TokenRefreshView, UserProfileViewSet, RegisterAPIView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name="token"),
     path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-    path('register/', register, name="register"),
-    path('activate/', activate, name="activate"),
-    path('profile/', UserProfileViewSet.as_view({'get': 'retrieve', 'post': 'create', 'put': 'update'})),
+    path('register/', RegisterAPIView.as_view(), name="register"),
+    path('verify/', confirm_registration_code, name="verify"),
+    path('profile/', UserProfileViewSet.as_view({
+        'get': 'retrieve',
+        'post': 'create',
+        'put': 'update',
+        'patch': 'partial_update'
+    })
+         ),
     path('has_profile/', UserProfileViewSet.as_view({'get': 'has_profile'})),
 ]
