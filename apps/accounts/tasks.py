e1ddf6dbc21d142ef5440d3b149e9974e31eb738
mehdi taherifard <m.taherifard@hami.blue>
2020-06-06 11:45:05 +0430

    add send_reset_password task

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -1,9 +1,12 @@
 import logging
 
+from django.core import mail
+from django.core.mail import EmailMessage
+
 from celery import shared_task
 from django.conf import settings
 
-from admood_core.settings import SITE_URL, EMAIL_VERIFICATION_URI
+from admood_core.settings import SITE_URL, USER_VERIFICATION_URL, USER_RESET_PASSWORD_URL
 from apps.core.utils.mail import Mail
 
 logger = logging.getLogger('admood_core.accounts')
@@ -11,11 +14,8 @@ logger = logging.getLogger('admood_core.accounts')
 
 @shared_task
 def send_verification_email(email_address, code):
-    verify_url = '{SITE_URL}/{EMAIL_VERIFICATION_URI}/?code={CODE}&user={EMAIL}' \
-        .format(SITE_URL=SITE_URL,
-                EMAIL_VERIFICATION_URI=EMAIL_VERIFICATION_URI,
-                CODE=code,
-                EMAIL=email_address)
+    verify_url = f'{SITE_URL}/{USER_VERIFICATION_URL}/?code={code}&user={email_address}'
+
     context = {
         'lang': 'fa',  # if settings.LANGUAGE_CODE.lower() == 'fa-ir' else 'en',
         'base_url': "127.0.0.1",
@@ -33,3 +33,13 @@ def send_verification_email(email_address, code):
                        template="accounts/email/verify.html",
                        context=context,
                        to_emails=[email_address])
+
+
+@shared_task
+def send_reset_password(email_address, code):
+    url = f'{SITE_URL}/{USER_VERIFICATION_URL}/{USER_RESET_PASSWORD_URL}/{code}'
+    message = EmailMessage('reset password', url, to=[email_address], from_email=settings.EMAIL_HOST_USER)
+    connection = mail.get_connection()
+    connection.open()
+    connection.send_messages([message])
+    connection.close()
