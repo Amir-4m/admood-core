732ea9722259585e1a898f87809316b87e7bbc47
mehdi taherifard <m.taherifard@hami.blue>
2020-06-06 11:45:39 +0430

    update User api views

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -1,6 +1,5 @@
-import datetime
-
 from django.contrib.auth import get_user_model
+from django.http import Http404
 from django.shortcuts import redirect
 from rest_framework.decorators import action
 from rest_framework.generics import GenericAPIView
@@ -11,12 +10,16 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenViewBase
 
-from admood_core.settings import SITE_URL, LOGIN_URI
+from admood_core.settings import SITE_URL, LOGIN_URL
 from apps.accounts.api.serializers import (
     MyTokenObtainPairSerializer,
     MyTokenRefreshSerializer,
     UserProfileSerializer,
-    RegisterSerializer, VerifyUserSerializer)
+    RegisterSerializer,
+    VerifyUserSerializer,
+    ResetPasswordSerializer,
+    ForgetPasswordSerializer,
+)
 from apps.accounts.models import UserProfile, Verification
 
 User = get_user_model()
@@ -35,52 +38,61 @@ class RegisterUserAPIView(GenericAPIView):
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            email = serializer.validated_data['email']
-            password = serializer.validated_data['password']
-
-            user = User.get_by_email(email)
-            if not user:
-                user = User.objects.create_user(
-                    email=email,
-                    password=password,
-                    is_active=False
-                )
-            else:
-                user.set_password(password)
-                user.save()
-            # create and email user verification code
-            user.email_verification_code()
-
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
 
 class VerifyUserAPIView(GenericAPIView):
     serializer_class = VerifyUserSerializer
+    queryset = Verification.objects.all()
+
+    def get_object(self):
+        code = self.request.query_params.get('code')
+        try:
+            return Verification.objects.get(code=code)
+        except:
+            raise redirect(f'{SITE_URL}/error/not-verified')
 
     def get(self, request):
+        verification = self.get_object()
+        if not verification.validate():
+            return redirect(f'{SITE_URL}/error/not-verified')
+        verification.verify()
+        verification.user.verify()
+        return redirect(f'{SITE_URL}/{LOGIN_URL}')
 
-        serializer = self.serializer_class(data=request.query_params)
-        if serializer.is_valid():
-            user = serializer.validated_data.get('user')
-            code = serializer.validated_data.get('code')
-            if Verification.verify_user(user, code):
-                return redirect(f'{SITE_URL}/{LOGIN_URI}')
 
-        return redirect(f'{SITE_URL}/error/not-verified')
+class ForgetPasswordAPIView(GenericAPIView):
+    serializer_class = ForgetPasswordSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
 class ResetPasswordAPIView(GenericAPIView):
-    serializer_class = RegisterSerializer
+    serializer_class = ResetPasswordSerializer
+
+    def get_object(self):
+        uuid = self.request.query_params.get('uuid')
+        try:
+            return Verification.get(uuid).user
+        except:
+            raise Http404
 
     def get(self, request):
-        pass
-        # todo: email user reset link
+        user = self.get_object()
+        return Response({'email': user.email})
 
-    def patch(self, request):
-        user = User.get_by_email("email")
-        serializer = self.serializer_class(instance=user, data=request.data, partial=True)
-
+    def put(self, request):
+        user = self.get_object()
+        serializer = self.serializer_class(instance=user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'email': user.email})
 
 
 class UserProfileViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
@@ -108,7 +120,6 @@ class UserProfileViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,
             serializer = self.get_serializer(instance)
             return Response(serializer.data)
         return Response({
-            'username': request.user.username,
             'phone_number': request.user.phone_number,
             'email': request.user.email
         })
