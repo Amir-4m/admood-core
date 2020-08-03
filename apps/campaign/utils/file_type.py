11b53eb56f0391f5f72460b02ff2c67b29ebca30
mehdi <m69fard@gmail.com>
2020-08-03 11:37:43 +0430

    add file_type to utils

---
+++ apps/campaign/utils/file_type.py
@@ -0,0 +1,17 @@
+PHOTO = 'photo'
+VIDEO = 'video'
+AUDIO = 'audio'
+DOCUMENT = 'document'
+
+FILE_TYPES = {
+    PHOTO: ['jpeg', 'jpg', 'png'],
+    VIDEO: ['mp4', 'gif', 'avi', 'wmv', 'mkv', 'flv', 'mov'],
+    AUDIO: ['mp3', 'wav', 'aac', 'ogg', 'wma'],
+}
+
+
+def file_type(extension):
+    for k, v in FILE_TYPES.items():
+        if extension in v:
+            return k
+    return DOCUMENT
