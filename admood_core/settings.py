365ac08c2c9da03ef7063fb794c6c2472f12b2ea
mehdi <m69fard@gmail.com>
2020-08-25 17:49:53 +0430

    remove pagination

--- admood_core/settings.py
+++ admood_core/settings.py
@@ -160,8 +160,8 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%Y-%m-%d %H:%M',
     'TIME_FORMAT': '%H:%M',
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 100,
 }
 
 AUTHENTICATION_BACKENDS = [
