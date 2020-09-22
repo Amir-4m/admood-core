33042fe5dcde96fde60077b4c11529b7014aeabf
mehdi <m69fard@gmail.com>
2020-09-22 14:27:10 +0330

    refactor update_telegram_publishers task

--- apps/medium/tasks.py
+++ apps/medium/tasks.py
@@ -1,23 +1,19 @@
-import requests
 from celery import shared_task
 
-from admood_core.settings import ADBOT_API_URL, ADBOT_API_TOKEN
 from apps.medium.consts import Medium
 from apps.medium.models import Publisher
+from services.telegram import get_publishers
 
 
 @shared_task
 def update_telegram_publishers():
-    headers = {'Authorization': ADBOT_API_TOKEN, }
-    channels_url = f'{ADBOT_API_URL}/api/v1/channels/'
-    r = requests.get(url=f'{channels_url}', headers=headers)
-    if not r.ok:
-        return
-    data = r.json()
-    for channel in data:
-        Publisher.objects.create(
+    channels = get_publishers()
+    for channel in channels:
+        Publisher.objects.update_or_create(
             reference_id=channel['id'],
             medium=Medium.TELEGRAM,
-            name=channel['title'],
-            is_enable=channel['is_enable']
+            defaults={
+                'name': channel['title'],
+                'is_enable': channel['is_enable']
+            }
         )
