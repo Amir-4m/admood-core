609463d351b15889c749f531662efe415cfc3e09
mehdi <m69fard@gmail.com>
2020-10-27 17:16:59 +0330

    add path 'register/phone/'

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -6,13 +6,12 @@ from .views import (MyTokenObtainPairView,
                     RegisterUserAPIView,
                     VerifyUserAPIView,
                     PasswordResetAPIView,
-                    PasswordResetConfirmAPIView, RegisterPhoneAPIView)
+                    PasswordResetConfirmAPIView)
 
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name="token"),
     path('token/refresh/', MyTokenRefreshView.as_view(), name="token_refresh"),
     path('register/', RegisterUserAPIView.as_view()),
-    path('register/phone/', RegisterPhoneAPIView.as_view()),
     path('register/verify/', VerifyUserAPIView.as_view()),
     path('reset-pass/', PasswordResetAPIView.as_view()),
     path('reset-pass/confirm/', PasswordResetConfirmAPIView.as_view()),
