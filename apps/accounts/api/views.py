6548b131a67f8cabe41389b5976cbce3bcf6bc3a
mehdi <m69fard@gmail.com>
2020-10-31 10:28:28 +0330

    update VerifyUserAPIView

--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.http import Http404
+from django.utils import timezone
 from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
@@ -9,6 +10,7 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from admood_core import settings
 from apps.accounts.api.serializers import (
     MyTokenObtainPairSerializer,
     MyTokenRefreshSerializer,
@@ -51,14 +53,26 @@ class RegisterPhoneAPIView(GenericAPIView):
 
 
 class VerifyUserAPIView(GenericAPIView):
-    serializer_class = VerifyUserSerializer
     queryset = Verification.objects.all()
 
+    def get_object(self):
+        code = self.request.data["code"]
+        try:
+            return self.queryset.objects.get(
+                code=code,
+                type=Verification.VERIFY_TYPE_EMAIL,
+                verified_time__isnull=True,
+                created_time__gt=timezone.now() - timezone.timedelta(minutes=settings.USER_VERIFICATION_LIFETIME),
+            )
+        except:
+            raise NotFound
+
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        verification = self.get_object()
+        verification.verify()
+        verification.save()
+        verification.user.verify()
+        return Response()
 
 
 class PasswordResetAPIView(GenericAPIView):
