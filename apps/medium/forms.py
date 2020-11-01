cdbc374fbbb63e26e9d821ddf538d387a0521651
mehdi <m69fard@gmail.com>
2020-11-01 13:58:56 +0330

    override __init__() for PublisherForm to handle field's queryset

--- apps/medium/forms.py
+++ apps/medium/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from apps.medium.models import Publisher
+from apps.medium.models import Publisher, Category, CostModelPrice
 
 
 class PublisherForm(forms.ModelForm):
     class Meta:
         model = Publisher
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['categories'].queryset = Category.objects.filter(medium=self.instance.medium)
+        self.fields['cost_models'].queryset = CostModelPrice.objects.filter(medium=self.instance.medium)
 
     def clean(self):
         categories = self.cleaned_data.get('categories', [])
@@ -17,3 +22,5 @@ class PublisherForm(forms.ModelForm):
             if category.medium != medium:
                 raise ValidationError({'categories': "category's medium doesn't match with publisher's medium."})
         return self.cleaned_data
+
+
