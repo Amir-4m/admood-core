c6119058106060d42bbbb80ddbab42612b7497cb
mehdi <m69fard@gmail.com>
2020-11-01 13:58:10 +0330

    update PublisherAdmin

--- apps/medium/admin.py
+++ apps/medium/admin.py
@@ -11,8 +11,15 @@ from .models import (
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ['name', 'medium']
-    search_fields = ['name']
+    list_filter = ['medium']
+    search_fields = ['name', 'medium']
+    fields = ["name", "medium", "categories", "cost_models", "description", "is_enable", "url", "ref_id"]
+    filter_horizontal = ['cost_models', 'categories']
+    readonly_fields = ['medium', 'ref_id', 'url']
     form = PublisherForm
+
+    def has_add_permission(self, request):
+        return False
 
 
 @admin.register(Category)
