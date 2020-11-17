e8958b319eceb21c2e59d652bd22473ca9faacc4
mehdi <m69fard@gmail.com>
2020-11-17 18:00:31 +0330

    add signals.py

---
+++ apps/campaign/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+from apps.campaign.models import Campaign
+from apps.payment.models import Transaction
+
+
+@receiver(post_save, sender=Campaign)
+def create_transaction(sender, instance, created, **kwargs):
+    if created:
+        return
+    if instance._b_status == Campaign.STATUS_WAITING and instance.status == Campaign.STATUS_REJECTED:
+        Transaction.objects.create(user=instance.owner,
+                                   value=instance.total_budget,
+                                   transaction_type=Transaction.TYPE_REFUND,
+                                   campaign=instance,
+                                   )
+
+    elif instance._b_status == Campaign.STATUS_REJECTED and instance.status in [Campaign.STATUS_WAITING,
+                                                                              Campaign.STATUS_APPROVED]:
+        Transaction.objects.create(user=instance.owner,
+                                   value=-instance.total_budget,
+                                   transaction_type=Transaction.TYPE_DEDUCT,
+                                   campaign=instance,
+                                   )
