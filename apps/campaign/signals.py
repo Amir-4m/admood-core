b7f94d1d7d40b5f4f6e3f8735b46bf1318be238d
mehdi <m69fard@gmail.com>
2020-11-21 13:08:38 +0330

    add else for created condition to signals

--- apps/campaign/signals.py
+++ apps/campaign/signals.py
@@ -15,25 +15,26 @@ def create_transaction(sender, instance, created, **kwargs):
                                        transaction_type=Transaction.TYPE_DEDUCT,
                                        campaign=instance,
                                        )
-    # when users ask to approve a campaign create a deduct transaction
-    if instance._b_status == Campaign.STATUS_DRAFT and instance.status == Campaign.STATUS_WAITING:
-        Transaction.objects.create(user=instance.owner,
-                                   value=-instance.total_budget,
-                                   transaction_type=Transaction.TYPE_DEDUCT,
-                                   campaign=instance,
-                                   )
-    # when a campaign rejected create a refund transaction
-    if instance._b_status == Campaign.STATUS_WAITING and instance.status == Campaign.STATUS_REJECTED:
-        Transaction.objects.create(user=instance.owner,
-                                   value=instance.total_budget,
-                                   transaction_type=Transaction.TYPE_REFUND,
-                                   campaign=instance,
-                                   )
-    # when a rejected campaign change to waiting or approve create a deduct transaction
-    elif instance._b_status == Campaign.STATUS_REJECTED and instance.status in [Campaign.STATUS_WAITING,
-                                                                                Campaign.STATUS_APPROVED]:
-        Transaction.objects.create(user=instance.owner,
-                                   value=-instance.total_budget,
-                                   transaction_type=Transaction.TYPE_DEDUCT,
-                                   campaign=instance,
-                                   )
+    else:
+        # when users ask to approve a campaign create a deduct transaction
+        if instance._b_status == Campaign.STATUS_DRAFT and instance.status == Campaign.STATUS_WAITING:
+            Transaction.objects.create(user=instance.owner,
+                                       value=-instance.total_budget,
+                                       transaction_type=Transaction.TYPE_DEDUCT,
+                                       campaign=instance,
+                                       )
+        # when a campaign rejected create a refund transaction
+        if instance._b_status == Campaign.STATUS_WAITING and instance.status == Campaign.STATUS_REJECTED:
+            Transaction.objects.create(user=instance.owner,
+                                       value=instance.total_budget,
+                                       transaction_type=Transaction.TYPE_REFUND,
+                                       campaign=instance,
+                                       )
+        # when a rejected campaign change to waiting or approve create a deduct transaction
+        elif instance._b_status == Campaign.STATUS_REJECTED and instance.status in [Campaign.STATUS_WAITING,
+                                                                                    Campaign.STATUS_APPROVED]:
+            Transaction.objects.create(user=instance.owner,
+                                       value=-instance.total_budget,
+                                       transaction_type=Transaction.TYPE_DEDUCT,
+                                       campaign=instance,
+                                       )
