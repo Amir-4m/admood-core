a8b3fee2e02f48ea61e6e561c004c22810b0855d
mehdi <m69fard@gmail.com>
2020-11-23 16:08:40 +0330

    override FileSerializer create to rename file name

--- apps/core/api/serializers.py
+++ apps/core/api/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
 from apps.core.models import File
@@ -7,3 +9,9 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = '__all__'
+
+    def create(self, validated_data):
+        file = validated_data['file']
+        ext = file.name.split('.')[-1]
+        file.name = f"{datetime.now().strftime('%Y-%m-%d')}_{self.context['request'].user.username}.{ext}"
+        return super(FileSerializer, self).create(validated_data)
