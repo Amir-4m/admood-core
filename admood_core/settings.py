297c9b4490eac966e34d5b7c02bbc3687e53e40a
amir <a.amiri@hami.blue>
2021-01-11 14:45:28 +0330

    locales updated

--- admood_core/settings.py
+++ admood_core/settings.py
@@ -184,7 +184,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 LANGUAGE_CODE = config("LANGUAGE_CODE", default="en-us")
 TIME_ZONE = 'Asia/Tehran'
-USE_I18N = False
+USE_I18N = True
 USE_L10N = False
 USE_TZ = False
 USE_THOUSAND_SEPARATOR = True
