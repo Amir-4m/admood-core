e44dcb63519630c5da3b673e4d6754459e92d9b7
Mehdi <m.rezaei@hami.blue>
2021-01-31 17:53:10 +0330

    finish_time changed to report_time, post save conditions cleaned

--- apps/campaign/signals.py
+++ apps/campaign/signals.py
@@ -19,61 +19,28 @@ def create_transaction(sender, instance, created, **kwargs):
         f'-[previous status: {instance._b_status}]-[current status: {instance.status}]'
     )
 
-    if created:
-        # when duplicate a campaign, create a deduct transaction
-        if instance.status == Campaign.STATUS_APPROVED:
-            Transaction.objects.create(user=instance.owner,
-                                       value=-instance.total_budget,
-                                       transaction_type=Transaction.TYPE_DEDUCT,
-                                       campaign=instance,
-                                       )
-    else:
-        # when users ask to approve a campaign, create a deduct transaction
-        if instance._b_status == Campaign.STATUS_DRAFT and instance.status == Campaign.STATUS_WAITING:
-            Transaction.objects.create(user=instance.owner,
-                                       value=-instance.total_budget,
-                                       transaction_type=Transaction.TYPE_DEDUCT,
-                                       campaign=instance,
-                                       )
-        # when a campaign rejected, create a refund transaction
-        if instance._b_status == Campaign.STATUS_WAITING and instance.status == Campaign.STATUS_REJECTED:
-            Transaction.objects.create(user=instance.owner,
-                                       value=instance.total_budget,
-                                       transaction_type=Transaction.TYPE_REFUND,
-                                       campaign=instance,
-                                       )
-        # when a rejected campaign change to waiting or approved status, create a deduct transaction
-        elif instance._b_status == Campaign.STATUS_REJECTED and instance.status in [Campaign.STATUS_WAITING,
-                                                                                    Campaign.STATUS_APPROVED]:
-            Transaction.objects.create(user=instance.owner,
-                                       value=-instance.total_budget,
-                                       transaction_type=Transaction.TYPE_DEDUCT,
-                                       campaign=instance,
-                                       )
+    trans_type = None
+
+    # if status changed
+    if instance._b_status != instance.status:
+        # if changed to waiting or approved
+        if instance.status in (Campaign.STATUS_WAITING, Campaign.STATUS_APPROVED):
+            trans_type = Transaction.TYPE_DEDUCT
+        # if changed to rejected
+        if instance.status == Campaign.STATUS_REJECTED:
+            trans_type = Transaction.TYPE_REFUND
+
+    # when duplicate a campaign, create a deduct transaction
+    if created and instance.status in (Campaign.STATUS_WAITING, Campaign.STATUS_APPROVED):
+        trans_type = Transaction.TYPE_DEDUCT
+
+    if trans_type:
+        Transaction.objects.create(user=instance.owner,
+                                   value=-instance.total_budget,
+                                   transaction_type=trans_type,
+                                   campaign=instance,
+                                   )
+
     # Updating Final Publisher field
     if instance._b_status == Campaign.STATUS_DRAFT and instance.status == Campaign.STATUS_WAITING:
-        # TODO Implement This
-        # publishers_by_categories_ids = Publisher.get_by_categories(
-        #     categories=instance.categories.all()
-        # ).values_list('id', flat=True)
-        # print(publishers_by_categories_ids)
-        # current_publisher_ids = instance.publisher_price.all().values_list('publisher_id')
-        # inserting_publisher = set(current_publisher_ids) - set(publishers_by_categories_ids)
-        #
-        # for publisher in Publisher.objects.filter(id__in=inserting_publisher):
-        #     try:
-        #         price = publisher.cost_models.filter(
-        #                 cost_model=CostModel.CPV
-        #             ).order_by('-publisher_price').first().publisher_price
-        #     except:
-        #         price = 0
-        #     instance.publisher_price.create(
-        #         publisher=publisher,
-        #         publisher_price=price
-        #     )
-        #
-        # current_publisher_ids = instance.publisher_price.all().values_list('publisher_id')
-        # deleting_publisher_ids = set(publishers_by_categories_ids) - set(current_publisher_ids)
-        # instance.publisher_price.filter(publisher_id__in=deleting_publisher_ids).delete()
-
         instance.update_final_publishers()
